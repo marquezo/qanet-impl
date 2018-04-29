345b9ab6dc9e368abeaccb24a200cb39e18eb31a
Orlando M <orlando.m.bol@gmail.com>
2018-04-29 15:13:38 -0400

    Completed the forward method

---
+++ qanet/word_embedding.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import constants
+
+class WordEmbedding(nn.Module):
+
+    # word_embeddings comes from numpy
+    def __init__(self, word_embeddings):
+        super(WordEmbedding, self).__init__()
+        self.word_embedding = nn.Embedding(word_embeddings.shape[0], word_embeddings.shape[1])
+        self.word_embedding.weight = nn.Parameter(torch.from_numpy(word_embeddings))
+
+        # Only the unknown embedding requires grad
+        self.word_embedding.weight.requires_grad = False
+        self.word_embedding.weight[constants.UNK_ID].requires_grad = True
+
+    def forward(self, input_context, input_query):
+        return self.word_embedding(input_context), self.word_embedding(input_query)
