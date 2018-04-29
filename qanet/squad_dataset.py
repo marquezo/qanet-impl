2187d44552fe3eadde2ac301dfeb6d69dbb10711
Orlando M <orlando.m.bol@gmail.com>
2018-04-29 18:25:52 -0400

    Also returns the span in the dataset

--- qanet/squad_dataset.py
+++ qanet/squad_dataset.py
@@ -8,7 +8,7 @@ from vocab_util import word2charix
 # (i.e., first return longer contexts)
 class SquadDataset(Dataset):
 
-    def __init__(self, file_ids_ctx, file_ids_q, file_ctx, file_q, char2ix_file):
+    def __init__(self, file_ids_ctx, file_ids_q, file_ctx, file_q, file_span, char2ix_file):
 
         #This will be used in the character mapping
         with open(char2ix_file) as json_data:
@@ -21,26 +21,31 @@ class SquadDataset(Dataset):
 
         list_context_raw = []
         list_question_raw = []
+        list_span = []
 
         with open(file_ids_q, "r") as question_file_ids, \
                 open(file_ids_ctx, "r") as context_file_ids, \
                 open(file_q, "r") as question_file, \
-                open(file_ctx, "r") as context_file:
+                open(file_ctx, "r") as context_file, \
+                open(file_span, "r") as span_file:
 
             # We need to find the max token length for questions
             # Also, collect the contexts and questions to sort them acc. to context length
             # (length in terms of tokens, NOT in terms of characters)
-            for context, question, context_raw, question_raw in \
-                    zip(context_file_ids, question_file_ids, context_file, question_file):
+            for context, question, context_raw, question_raw, spans in \
+                    zip(context_file_ids, question_file_ids, context_file, question_file, span_file):
 
                 context_array = np.fromstring(context.strip('\n'), dtype=int, sep=' ')
                 question_array = np.fromstring(question.strip('\n'), dtype=int, sep=' ')
+                span_array = np.fromstring(spans.strip('\n'), dtype=int, sep=' ')
 
                 list_context_raw.append(context_raw.strip('\n'))
                 list_question_raw.append(question_raw.strip('\n'))
 
                 list_context.append(context_array)
                 list_questions.append(question_array)
+
+                list_span.append(span_array)
 
                 if question_array.shape[0] > self.max_question_len:
                     self.max_question_len = question_array.shape[0]
@@ -53,23 +58,34 @@ class SquadDataset(Dataset):
         self.question_sorted = []
         self.context_raw_sorted = []
         self.question_raw_sorted = []
+        self.spans_sorted = []
 
         for i in pos_for_batch:
             self.context_sorted.append(list_context[i])
             self.question_sorted.append(list_questions[i])
             self.context_raw_sorted.append(list_context_raw[i])
             self.question_raw_sorted.append(list_question_raw[i])
+            self.spans_sorted.append(list_span[i])
 
     def __len__(self):
         return len(self.context_sorted)
 
+    #Return:
+    # 400 dimensional vector containing the indices of context tokens in the word embedding space
+    # 60 dimensional vector containing the indices of question tokens in the word embedding space
+    # 400x16 matrix containing the indices of context tokens in the char embedding space (16d for each word)
+    # 60x16 matrix containing the indices of question tokens in the char embedding space (16d for each word)
+    # 2 dimensional vector containing the start and end token position of the answer in the context
+    # Raw context content (for debugging)
+    # Raw question content (for debugging)
+    #
     def __getitem__(self, idx):
 
-        context_pad = np.array([constants.PAD_ID] * (constants.MAX_CONTEXT - self.context_sorted[idx].shape[0]))
-        question_pad = np.array([constants.PAD_ID] * (self.max_question_len - self.question_sorted[idx].shape[0]))
+        context_pad_word = np.array([constants.PAD_ID] * (constants.MAX_CONTEXT - self.context_sorted[idx].shape[0]))
+        question_pad_word = np.array([constants.PAD_ID] * (self.max_question_len - self.question_sorted[idx].shape[0]))
 
-        context_to_return = np.concatenate((self.context_sorted[idx], context_pad))
-        question_to_return = np.concatenate((self.question_sorted[idx], question_pad))
+        context_word_idx = np.concatenate((self.context_sorted[idx], context_pad_word))
+        question_word_idx = np.concatenate((self.question_sorted[idx], question_pad_word))
 
         #We need to return the character mapping also
         context_raw = self.context_raw_sorted[idx]
@@ -84,10 +100,10 @@ class SquadDataset(Dataset):
         q4char = np.array(q4char)
 
         #Pad character embedding
-        pad4ctx = np.zeros((constants.MAX_CONTEXT - ctx4char.shape[0], ctx4char.shape[1]))
-        pad4q = np.zeros((self.max_question_len - q4char.shape[0], q4char.shape[1]))
+        context_pad_char = np.zeros((constants.MAX_CONTEXT - ctx4char.shape[0], ctx4char.shape[1]))
+        question_pad_char = np.zeros((self.max_question_len - q4char.shape[0], q4char.shape[1]))
 
-        char_mapping_context = np.concatenate((ctx4char, pad4ctx))
-        char_mapping_question =  np.concatenate((q4char, pad4q))
+        context_char_idx = np.concatenate((ctx4char, context_pad_char))
+        question_char_idx =  np.concatenate((q4char, question_pad_char))
 
-        return context_to_return, question_to_return, char_mapping_context, char_mapping_question
+        return context_word_idx, question_word_idx, context_char_idx, question_char_idx, self.spans_sorted[idx], context_raw, question_raw
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from qanet.squad_dataset import SquadDataset
 from torch.utils.data import DataLoader
 import json
 from qanet.input_embedding import InputEmbedding
+from qanet.residual_block import ResidualBlock
 
 data_prefix = 'data/'
 save_path = data_prefix + 'glove.trimmed.300d.npz'
@@ -13,18 +14,26 @@ with open(char2ix_file) as json_data:
 
 embeddings = np.load(save_path)['glove']
 
+dataset = SquadDataset(file_ids_ctx=data_prefix + 'train.context.ids', file_ids_q=data_prefix + 'train.question.ids',
+                      file_ctx =data_prefix + 'train.context', file_q=data_prefix + 'train.question', file_span=data_prefix + 'train.span', char2ix_file=char2ix_file)
+
+train_loader = DataLoader(dataset, batch_size=2, shuffle=False, num_workers=1)
+
+
 input_embedding_layer = InputEmbedding(embeddings, len(char2ix))
 
-dataset = SquadDataset(file_ids_ctx=data_prefix + 'train.context.ids', file_ids_q=data_prefix + 'train.question.ids',
-                      file_ctx =data_prefix + 'train.context', file_q=data_prefix + 'train.question', char2ix_file=char2ix_file)
+embedding_encoder_layer = ResidualBlock(num_blocks=1, num_conv_layers=4, kernel_size=7, mask=None,
+                 num_filters=128, input_projection=True, num_heads=8,
+                 seq_len=None, is_training=True, bias=True, dropout=0.0)
 
-train_loader = DataLoader(dataset, batch_size=128, shuffle=False, num_workers=1)
-
-for batch_idx, (context_word, question_word, context_char, question_char) in enumerate(train_loader):
+for batch_idx, (context_word, question_word, context_char, question_char, spans, ctx_raw, q_raw) in enumerate(train_loader):
 
     input_context, input_question = input_embedding_layer(context_word, question_word, context_char.long(), question_char.long())
 
-    print (input_context.shape)
-    print (input_question.shape)
+    input_context = embedding_encoder_layer(input_context)
+    # input_question = embedding_encoder_layer(input_question)
+
+    # print (input_context.shape)
+    # print (input_question.shape)
 
     break
