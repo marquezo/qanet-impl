d09cc2f8460af19f5b40a1435a75ebeba1f2219a
Orlando M <orlando.m.bol@gmail.com>
2018-04-29 15:07:20 -0400

    Also do the character pre-processing

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import numpy as np
 import os
 import random
 from parse_json import data_from_json, read_write_dataset
-from vocab_util import create_vocabulary, initialize_vocabulary, process_glove, data_to_token_ids
+from vocab_util import create_vocabulary, initialize_vocabulary, process_glove, data_to_token_ids, create_vocab2charix_dict
+import json
 
 random.seed(42)
 np.random.seed(42)
 
-train_path = '/home/orlandom/datasets/train-v1.1.json'
-dev_path = '/home/orlandom/datasets/dev-v1.1.json'
-glove_file_path = '/home/orlandom/Downloads/glove.6B/glove.6B.300d.txt'
+root_project = '/home/orlandom/'
+
+train_path = root_project + '/datasets/train-v1.1.json'
+dev_path = root_project + '/datasets/dev-v1.1.json'
+glove_file_path = root_project + '/Downloads/glove.6B/glove.6B.300d.txt'
 data_prefix = 'data/'
 save_path = data_prefix + 'glove.trimmed.300d.npz'
 vocab_path = data_prefix + 'vocab'
@@ -19,7 +22,9 @@ train_question_path = data_prefix + 'train.question'
 train_context_ids_path = train_context_path + ".ids"
 train_question_ids_path = train_question_path + ".ids"
 
-
+######################################################################################################################
+# Do vocabulary and word pre-processing
+#####################################################################################################################
 train_data = data_from_json(train_path)
 dev_data = data_from_json(dev_path)
 
@@ -38,3 +43,33 @@ process_glove(glove_file_path, rev_vocab, save_path)
 
 data_to_token_ids(train_context_path, train_context_ids_path, vocab_path)
 data_to_token_ids(train_question_path, train_question_ids_path, vocab_path)
+
+######################################################################################################################
+# Do character pre-processing
+#####################################################################################################################
+char2ix = {'<pad>': '0'}
+ix2char = {'0': '<pad>'}
+char2ix_file = data_prefix + 'char2ix.json'
+ix2char_file = data_prefix + 'ix2char.json'
+vocab2charix_file = data_prefix + 'vocab2charix.json'
+
+with open(vocab_path) as f:
+    i = len(char2ix)
+    for line in f:
+        line = line.strip()
+        if line in ['<pad>', '<sos>', '<unk>']:
+            continue
+        for char in line:
+            if char not in char2ix:
+                char2ix[char] = str(i)
+                ix2char[str(i)] = char
+                i += 1
+
+
+with open(char2ix_file, 'w') as f:
+    json.dump(char2ix, f)
+
+with open(ix2char_file, 'w') as f:
+    json.dump(ix2char, f)
+
+create_vocab2charix_dict(vocab_path, vocab2charix_file, char2ix)
