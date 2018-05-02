1ff1de2af569335b323674ab83406434d907ba3f
Vachonni <nicholasvachon@hotmail.com>
2018-05-02 17:17:16 -0400

    Added functions to download Glove and SQuUAD

--- download.py
+++ download.py
@@ -3,13 +3,44 @@
 
 def download_glove():
     
-    pass
+    import zipfile
+
+    !wget http://nlp.stanford.edu/data/glove.6B.zip
+    
+    zip_ref = zipfile.ZipFile('glove.6B.zip.1', 'r')
+    zip_ref.extractall()
+    zip_ref.close()
+    
+# =============================================================================
+#     import zipfile
+#     from squad_preprocess import *
+#     
+#     if __name__ == '__main__':
+#         glove_base_url = "http://nlp.stanford.edu/data/"
+#         glove_filename = "glove.6B.zip"
+#         prefix = os.path.join("download", "dwr")
+#     
+#         print("Storing datasets in {}".format(prefix))
+#     
+#         if not os.path.exists(prefix):
+#             os.makedirs(prefix)
+#     
+#         glove_zip = maybe_download(glove_base_url, glove_filename, prefix, 862182613L)
+#         glove_zip_ref = zipfile.ZipFile(os.path.join(prefix, glove_filename), 'r')
+#     
+#         glove_zip_ref.extractall(prefix)
+#         glove_zip_ref.close()
+# =============================================================================
+
 
 def download_squad():
     
-    pass
+    !wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json
+    !wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json
+
+
 
 if __name__ == "__main__":
-    
+  
     download_glove()
     download_squad()
